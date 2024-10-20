30950036ab0dab653e8ced7032faf98cbd67e807
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2024-10-21 01:42:43 +0900

    Added lang generator Changed LICENCE

--- src/client/java/karasu_lab/mcmidi/mixin/client/SoundManagerAccessor.java
+++
@@ -1,8 +0,0 @@
-package karasu_lab.mcmidi.mixin.client;
-
-import karasu_lab.mcmidi.options.MidiSound;
-import net.minecraft.sound.SoundCategory;
-
-public interface SoundManagerAccessor {
-    void updateMidiVolume(MidiSound category, float volume);
-}
---
+++ src/main/java/karasu_lab/mcmidi/data/ModLangProviders.java
@@ -0,0 +1,39 @@
+package karasu_lab.mcmidi.data;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModLangProviders {
+    public static void addProviders(FabricDataGenerator.Pack pack) {
+        pack.addProvider(EnglishLang::new);
+        pack.addProvider(JapaneseLangProvider::new);
+    }
+
+    public static class JapaneseLangProvider extends FabricLanguageProvider {
+        protected JapaneseLangProvider(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+            super(dataOutput, "ja_jp", registryLookup);
+        }
+
+        @Override
+        public void generateTranslations(RegistryWrapper.WrapperLookup registryLookup, TranslationBuilder translationBuilder) {
+
+        }
+    }
+
+    public static class EnglishLang extends FabricLanguageProvider {
+        protected EnglishLang(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+            super(dataOutput, "en_us", registryLookup);
+        }
+
+        @Override
+        public void generateTranslations(RegistryWrapper.WrapperLookup registryLookup, TranslationBuilder translationBuilder) {
+            translationBuilder.add("options.midi", "MIDI Settings");
+            translationBuilder.add("text.autoconfig.mcmidi.title", "MCMidi");
+            translationBuilder.add("text.autoconfig.mcmidi.options.midiVolume", "MIDI Volume");
+        }
+    }
+}
